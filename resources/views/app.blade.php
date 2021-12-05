470c31eb4f8c2f951dd88e9482f922c7bd8a673b
ShinHyungJune <ssa4141@naver.com>
2021-12-05 18:57:33 +0900

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,46 +17,48 @@
     <script src="{{ asset('js/common.js') }}" defer></script>
     <script src="{{ mix('/js/app.js') }}" defer></script>
     <!-- Channel Plugin Scripts -->
-    <script>
-        (function() {
-            var w = window;
-            if (w.ChannelIO) {
-                return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
-            }
-            var ch = function() {
-                ch.c(arguments);
-            };
-            ch.q = [];
-            ch.c = function(args) {
-                ch.q.push(args);
-            };
-            w.ChannelIO = ch;
-            function l() {
-                if (w.ChannelIOInitialized) {
-                    return;
-                }
-                w.ChannelIOInitialized = true;
-                var s = document.createElement('script');
-                s.type = 'text/javascript';
-                s.async = true;
-                s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
-                s.charset = 'UTF-8';
-                var x = document.getElementsByTagName('script')[0];
-                x.parentNode.insertBefore(s, x);
-            }
-            if (document.readyState === 'complete') {
-                l();
-            } else if (window.attachEvent) {
-                window.attachEvent('onload', l);
-            } else {
-                window.addEventListener('DOMContentLoaded', l, false);
-                window.addEventListener('load', l, false);
-            }
-        })();
-        ChannelIO('boot', {
-            "pluginKey": "46d45f61-d50a-45af-a52a-40645e5d1173"
-        });
-    </script>
+    <!-- Channel Plugin Scripts -->
+<script>
+  (function() {
+    var w = window;
+    if (w.ChannelIO) {
+      return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+    }
+    var ch = function() {
+      ch.c(arguments);
+    };
+    ch.q = [];
+    ch.c = function(args) {
+      ch.q.push(args);
+    };
+    w.ChannelIO = ch;
+    function l() {
+      if (w.ChannelIOInitialized) {
+        return;
+      }
+      w.ChannelIOInitialized = true;
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+      s.charset = 'UTF-8';
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    }
+    if (document.readyState === 'complete') {
+      l();
+    } else if (window.attachEvent) {
+      window.attachEvent('onload', l);
+    } else {
+      window.addEventListener('DOMContentLoaded', l, false);
+      window.addEventListener('load', l, false);
+    }
+  })();
+  ChannelIO('boot', {
+    "pluginKey": "e0da42ff-9b63-4259-aee3-060b70cfd459"
+  });
+</script>
+<!-- End Channel Plugin -->
     <!-- End Channel Plugin -->
     <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
 
